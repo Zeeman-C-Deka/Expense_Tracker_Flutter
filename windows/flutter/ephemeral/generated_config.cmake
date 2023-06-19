5a19251b142b43998b92dd8c6b1f298f1b735db3
Zeeman-C-Deka <79737506+Zeeman-C-Deka@users.noreply.github.com>
2023-06-20 02:10:41 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "G:\\SDK\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\Android\\Flutter course\\05\\expense_tracker" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=G:\\SDK\\flutter"
+  "PROJECT_DIR=F:\\Android\\Flutter course\\05\\expense_tracker"
+  "FLUTTER_ROOT=G:\\SDK\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\Android\\Flutter course\\05\\expense_tracker\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\Android\\Flutter course\\05\\expense_tracker"
+  "FLUTTER_TARGET=F:\\Android\\Flutter course\\05\\expense_tracker\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\Android\\Flutter course\\05\\expense_tracker\\.dart_tool\\package_config.json"
+)
